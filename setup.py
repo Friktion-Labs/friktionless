cd6d31ad668e4c69137bc56d978a9a0a3e442ff3
Matt Martin <matt.martin@hey.com>
2022-05-18 22:13:20 -0600

    added packages field back in

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
    name='friktionless',
-   version='0.1',
+   version='0.0.1',
    description='A utility library for the friktion team to automate data analysis and infrastructure',
    author='Matt Martin',
    author_email='matt@friktionlabs.com',
-   #packages=['friktionless'], 
+   packages=['friktionless'], 
    install_requires=['altair','pandas','google-cloud-storage']
 )
